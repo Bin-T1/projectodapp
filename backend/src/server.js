7df033391a669b5ee38ede381616cd5a89054abb
Chien <haminhchien7979@gmail.com>
2025-10-01 19:27:57 +0700

    kk

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,24 +13,26 @@ const __dirname = path.resolve();
 const app = express();
 
 
-//MiddelWares
-if(process.env.NODE_ENV !== "production"){
-    app.use(cors({origin:"http://localhost:5173"})) 
+
+// middlewares
+app.use(express.json());
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
 }
-app.use(express.json());// Dòng này kiểm tra là middel wel 
-app.use("/api/tasks",tasksRoute);
 
-if(process.env.NODE_ENV === 'production'){
-    
-app.use(express.static(path.join(__dirname,"../frontend/dist")));
+app.use("/api/tasks", tasksRoute);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
-})
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 }
+
+
+
 connectDB().then(()=>{
 app.listen(PORT,()=>{
     console.log(`Server đang chạy trên cổng ${PORT}`);
